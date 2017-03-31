3c9f08a1d29dc47fc3ea138d530a99054e9dcbb1
Judit Juhasz <judit-juhasz@users.noreply.github.com>
2017-03-31 06:13:49 +0200

    Prepare for starting a new activity

--- app/src/main/java/com/example/android/flashcards/ConfigActivity.java
+++ app/src/main/java/com/example/android/flashcards/ConfigActivity.java
@@ -2,6 +2,8 @@ package com.example.android.flashcards;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class ConfigActivity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class ConfigActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_config);
+    }
+
+    public void startQuizOnClick(View view) {
+        Toast.makeText(this, "The button is working!", Toast.LENGTH_SHORT).show();
     }
 }
