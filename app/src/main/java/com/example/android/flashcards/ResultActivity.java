0f97d9416d3c9d95966f4a76214cc9edbe711403
Judit Juhasz <judit-juhasz@users.noreply.github.com>
2017-04-02 22:49:42 +0200

    Add skeleton for the result activity

--- app/src/main/java/com/example/android/flashcards/ResultActivity.java
+++ app/src/main/java/com/example/android/flashcards/ResultActivity.java
@@ -1,7 +1,9 @@
 package com.example.android.flashcards;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ResultActivity extends AppCompatActivity {
 
@@ -15,5 +17,23 @@ public class ResultActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+
+        final Intent intent = getIntent();
+
+        final TextView congratulationsTextView = (TextView) findViewById(R.id.tv_congratulations);
+        if (intent.hasExtra(EXTRA_NAME)) {
+            congratulationsTextView.setText("Congratulations, " + intent.getStringExtra(EXTRA_NAME)
+                    + "!");
+        } else {
+            congratulationsTextView.setText("Congratulations!");
+        }
+
+        final int numberOfQuestions = intent.getIntExtra(EXTRA_NUMBER_OF_QUESTIONS, 1);
+        final int numberOfCorrectAnswers =
+                intent.getIntExtra(EXTRA_NUMBER_OF_CORRECT_ANSWERS, numberOfQuestions);
+        final int resultPercentage = (int) ((numberOfCorrectAnswers * 100.0) / numberOfQuestions);
+        final TextView resultTextView = (TextView) findViewById(R.id.tv_result);
+        resultTextView.setText("Your result is " + resultPercentage + "% which is not that bad!");
+
     }
 }
